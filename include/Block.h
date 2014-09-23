e5b12aa888ccaea906703dd5add228ddef61f5f3
elekt <elekestamas22@gmail.com>
2014-09-23 11:34:26 +0200

    structural rearrange, Block class

---
+++ include/Block.h
@@ -0,0 +1,32 @@
+#ifndef BLOCK_H
+#define BLOCK_H
+
+#include <vector>
+#include <utility>
+#include "Map.h"
+
+// kicsit olvashatobba teszi a kodot
+typedef std::pair<int, int> Point;
+
+/**
+* Tarol egy lerakhato jatek elemet.
+* o felel forgatasert, tukrozesert, palyan valo koordinatai kiszamolasaert
+**/
+class Block
+{
+  public:
+    Block();
+    virtual ~Block();
+    Block(const Block& other);
+
+    std::vector<Point> placeOnMap(Map& map);
+    void turn(int degrees);
+    void mirror(int sides);
+
+  protected:
+  private:
+    std::vector<Point> points;
+
+};
+
+#endif // BLOCK_H
---
+++ src/Block.cpp
@@ -0,0 +1,13 @@
+#include "Block.h"
+
+Block::Block()
+{
+}
+
+Block::~Block()
+{
+}
+
+Block::Block(const Block& other)
+{
+}
