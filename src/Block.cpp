07d9d51b38801b9eefc80cbd0f436456c0cea110
elekt <elekestamas22@gmail.com>
2014-09-24 11:57:24 +0200

    Block::turn beallitja szog alapjan az iranyvektort

--- src/Block.cpp
+++ src/Block.cpp
@@ -21,10 +21,35 @@ std::vector<Point> Block::placeOnMap(Point startPoint){
     return pointsOnMap;
 }
 
-void turn(int degrees){
+/// egyelore csak elv, iranyt is be kene majd allitani
+/// szogekben kapja meg a forgatast
+void Block::turn(int degrees){
+    Point dVec = Point(0,0);
+    bool bSwitch;
+    switch(degrees){
+        case 90:
+            bSwitch = true;
+            dVec = Point(-1, 1);
+            break;
+        case 180:
+            bSwitch = false;
+            dVec = Point(-1,-1);
+            break;
+        case 270:
+            bSwitch = true;
+            dVec = Point(-1, 1);
+            break;
+        default:
+            std::cout<< "Wrong turning input! Block::turn" << std::endl;
+            return;
+            break;
+    }
 
+    for ( auto iter = points.begin(); iter != points.end(); iter++ ) {
+
+    }
 }
 
-void mirror(int sides){
+void Block::mirror(int sides){
 
 }
