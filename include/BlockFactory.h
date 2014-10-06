b2741d80dec172d85f079ae8d4ec9936a89bf9c9
fukszdomonkos <fukszdomonkos@gmail.com>
2014-10-06 20:32:13 +0200

    block keszito fgv deklaraciok. pointerek bug

--- include/BlockFactory.h
+++ include/BlockFactory.h
@@ -2,9 +2,34 @@
 #define BLOCKFACTORY_H
 
 
+#include "Block.h"
+#include <vector>
 class BlockFactory
 {
     public:
+        static std::vector<Block> createAllBlocks(int color);
+        ///naming according to http://www.boardgamegeek.com/image/112331/blokus
+        static Block create1O(int color);
+        static Block create2I(int color);
+        static Block create3I(int color);
+        static Block create3L(int color);
+        static Block create4I(int color);
+        static Block create4T(int color);
+        static Block create4Z(int color);
+        static Block create4O(int color);
+        static Block create5I(int color);
+        static Block create5L(int color);
+        static Block create5U(int color);
+        static Block create5Z(int color);
+        static Block create5T(int color);
+        static Block create5X(int color);
+        static Block create5W(int color);
+        static Block create5V(int color);
+        static Block create5F(int color);
+        static Block create5P(int color);
+        static Block create5Y(int color);
+        static Block create5N(int color);
+
 
     protected:
     private:
--- src/BlockFactory.cpp
+++ src/BlockFactory.cpp
@@ -1,11 +1,59 @@
 #include "BlockFactory.h"
 
-BlockFactory::BlockFactory()
-{
-    //ctor
-}
+ std::vector<Block> BlockFactory::createAllBlocks(int color){
+    std::vector<Block> blocks;
+    blocks.push_back(create1O(color));
+    blocks.push_back(create1O(color));
 
-BlockFactory::~BlockFactory()
-{
-    //dtor
+
+    return blocks;
+ }
+Block BlockFactory::create1O(int color){
+    std::vector<Point> points;
+    points.push_back(Point(0,0));
+
+
+    return Block(points, color);
 }
+Block BlockFactory::create2I(int color){
+
+}
+Block BlockFactory::create3I(int color);
+Block BlockFactory::create3L(int color);
+Block BlockFactory::create4I(int color);
+Block BlockFactory::create4T(int color);
+Block BlockFactory::create4Z(int color);
+Block BlockFactory::create4O(int color);
+Block BlockFactory::create5I(int color);
+Block BlockFactory::create5L(int color);
+Block BlockFactory::create5U(int color);
+Block BlockFactory::create5Z(int color);
+Block BlockFactory::create5T(int color);
+Block BlockFactory::create5X(int color);
+Block BlockFactory::create5W(int color);
+Block BlockFactory::create5V(int color);
+Block BlockFactory::create5F(int color);
+Block BlockFactory::create5P(int color);
+Block BlockFactory::create5Y(int color);
+Block BlockFactory::create5N(int color);
+
+
+
+
+
+
+
+
+
+
+
+
+//BlockFactory::BlockFactory()
+//{
+//    //ctor
+//}
+//
+//BlockFactory::~BlockFactory()
+//{
+//    //dtor
+//}
